1b2e2971c6e8196ba2202765a90e9c1b8db13ff0
Miguelito0009 <miguel.tangarife@upb.edu.co>
2026-08-06 09:33:42 -0500

    Variables Constantes

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui va el codigo fuente
+            //Variables
+            string nombre = "Miguel";
+            Console.WriteLine("bienvenido " + nombre+"al curso de Fundamentos de Programacion");
+        }
+    }
+}
