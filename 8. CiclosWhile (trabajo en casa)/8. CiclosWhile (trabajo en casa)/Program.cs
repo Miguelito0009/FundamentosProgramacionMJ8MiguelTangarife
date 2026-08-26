f427d7de3ac40a83322707138c9531182508a3ef
Miguelito0009 <miguel.tangarife@upb.edu.co>
2026-08-25 22:51:05 -0500

    Trabajo en casa propuesto clase

---
+++ 8. CiclosWhile (trabajo en casa)/8. CiclosWhile (trabajo en casa)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace _8.CiclosWhile__trabajo_en_casa_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int acumulador = 0;
+
+            Console.WriteLine("Ingrese un numero entero positivo para sumar: ");
+            Console.WriteLine("ingrese un numero negativo para terminar: ");
+
+            num = int.Parse(Console.ReadLine());
+
+            while (num >= 0)
+            {
+                acumulador += num;
+                Console.WriteLine("Ingrese un numero entero positivo para sumar ");
+                Console.WriteLine("ingrese un numero negativo para terminar: ");
+                num = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("La suma total de los numeros ingresados es: " + acumulador);
+
+        }
+    }
+}
