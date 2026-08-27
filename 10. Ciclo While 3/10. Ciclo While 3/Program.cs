28c6e70fd8d75e674ffa438289b878d83e86b7e1
Miguelito0009 <miguel.tangarife@upb.edu.co>
2026-08-27 10:33:21 -0500

    10. Ciclo while trabajo propuesto clase

---
+++ 10. Ciclo While 3/10. Ciclo While 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Random random = new Random();
+        int numeroSecreto = random.Next(1, 101);
+        int intento = 0;
+
+        while (intento != numeroSecreto)
+        {
+            Console.Write("Adivina el número (1 - 100): ");
+            intento = int.Parse(Console.ReadLine());
+
+            if (intento > numeroSecreto)
+            {
+                Console.WriteLine("Demasiado alto.");
+            }
+            else if (intento < numeroSecreto)
+            {
+                Console.WriteLine("Demasiado bajo.");
+            }
+            else
+            {
+                Console.WriteLine("¡Correcto! Has adivinado el número.");
+            }
+        }
+    }
+}
